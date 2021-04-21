a68e120443cbc3eb6b97ae90397ae675ccd9d67f
Yokk1e <satthabut.o@gmail.com>
2021-04-21 18:59:24 +0700

    - [feat] move file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { DocumentsModule } from './documents/documents.module';
+import { DocumentsController } from './documents/documents/documents.controller';
+import { DocumentsService } from './documents/documents/documents.service';
 
 @Module({
   imports: [TypeOrmModule, ConfigModule, DocumentsModule],
--- src/documents/documents.module.ts
+++ src/documents/documents.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { DocumentsController } from './documents.controller';
-import { DocumentsService } from './documents.service';
+import { DocumentsController } from './documents/documents.controller';
+import { DocumentsService } from './documents/documents.service';
 
 @Module({
   controllers: [DocumentsController],
-  providers: [DocumentsService]
+  providers: [DocumentsService],
 })
 export class DocumentsModule {}
