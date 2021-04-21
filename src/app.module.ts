52bfd990688f98756f0966d267f7e65a2a4a5d28
Yokk1e <satthabut.o@gmail.com>
2021-04-21 18:49:04 +0700

    - [chore] add lib

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DocumentsModule } from './documents/documents.module';
 
 @Module({
-  imports: [],
+  imports: [DocumentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
